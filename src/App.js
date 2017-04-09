9fc0306b148e35b547a8ea62a0f1e406f901ee55
scottdomes <scottdomes@gmail.com>
2017-04-08 17:48:59 -0700

    Set up TweetStream

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = { tweets: [], loading: true }
+
+  componentDidMount() {
+    setTimeout(() => {
+      fetch('tweets.json')
+        .then(res => {
+          res.json().then(data => {
+            this.setState({ tweets: data.tweets, loading: false })
+          })
+        }).catch(err => console.log(err))
+    }, 2000)
+  }
+
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+    if (this.state.loading) {
+      return (
+        <div id="TweetStream">
+          <div className="loading-indicator">
+            <div/>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      )
+    }
+    return (
+      <div id="TweetStream">
+        {
+          this.state.tweets.map(tweet => {
+            return (
+              <div className="tweet" key={tweet.id}>
+                <img className="profile" alt="Profile" src={tweet.profile_pic_url} />
+                <div className="tweet-body">
+                  <div className="name-fields">
+                    <h1>{tweet.full_name}</h1>
+                    <h2>@{tweet.username}</h2>
+                  </div>
+                  <p>{tweet.text}</p>
+                  <img className="attached" src={tweet.attached_image} alt="Attached" />
+                </div>
+              </div>
+            )
+          })
+        }
       </div>
     );
   }
